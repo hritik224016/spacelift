c342f730949030062fd3f4eb9f048b2743e49a92
hritik224016 <hritik.224016@gmail.com>
2025-03-10 12:10:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "ec2_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
   key_name = var.aws_keypair
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id ]
 }
