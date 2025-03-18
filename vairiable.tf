f1d524b732e2befbb6090814bd91e6d74b6046e7
hritik224016 <hritik.224016@gmail.com>
2025-03-18 14:33:49 +0530

    Update vairiable.tf

--- vairiable.tf
+++ vairiable.tf
@@ -1,19 +1,19 @@
 variable "instance_name" {
   type = string
-  default = ""
+  default = "awsInstance"
 }
 
 variable "secret_id" {
     type = string
-    default =""
+    default ="F43UokOL2Udtz73a7qNPaWQ9kloJicYyd8jlkYC4"
 }
 variable "access_id" {
   type = string
-  default =""
+  default ="AKIAQXPZDIK42IXVLSXR"
 }
 variable "ami_id" {
   type = string
-  default = ""
+  default = "ami-04b4f1a9cf54c11d0"
 }
 
 variable "instance_type" {
@@ -23,16 +23,16 @@ variable "instance_type" {
 
 variable "aws_keypair" {
   type = string
-  default = ""
+  default = "keyid"
 }
 
 variable "region" {
   type = string
-  default = ""
+  default = "us-east-1"
 }
 variable "vpc_existing_id" {
   type = string
-  default = ""
+  default = "vpc-05ee27b83d616e22e"
 }
 
 variable "security_group_ids" {
